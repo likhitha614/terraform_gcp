e6c9e23818d808688eee85bd887b0d6e352c4de6
likhitha614 <likhitha614@gmail.com>
2018-08-09 21:39:01 -0400

    added new lines

--- terraform_gcp.tf
+++ terraform_gcp.tf
@@ -3,9 +3,21 @@ resource "google_compute_instance" "blog" {
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
-  disk {
-    image = "debian-cloud/debian-8"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8"
+    }
   }
+
+  disk {
+    type    = "local-ssd"
+    scratch = true
+  }
+
+  network_interface {
+    network = "default"
+  }
+}  }
 
   disk {
     type    = "local-ssd"
